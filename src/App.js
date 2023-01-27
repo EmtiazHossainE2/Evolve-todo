f56b5e9725923c2abc30a9e8ff21c0656a30abee
EmtiazHossainE2 <emtiazemon310@gmail.com>
2023-01-27 23:54:08 +0600

    initial setup with react and tailwind css

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h2 className="text-center text-red-500 text-4xl">Hello Todo</h2>
+    </main>
+  )
+}
+
+export default App
